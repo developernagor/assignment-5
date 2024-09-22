ebcc57cce5dc7f147bfa41bac97919eba879cfbf
developernagor <developernagor@gmail.com>
2024-09-22 22:42:24 +0600

    Initial Project setup

---
+++ js/tailwind.config.js
@@ -0,0 +1,12 @@
+    <script>
+        tailwind.config = {
+        theme: {
+            extend: {
+            colors: {
+                primary: 'rgb(17, 17, 17)',
+                primary_bg: 'rgb(180, 244, 97)',
+            }
+            }
+        }
+        }
+    </script>
