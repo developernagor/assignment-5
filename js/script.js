fb5344270af240ee3fecd05a3ef696c1cef77598
developernagor <developernagor@gmail.com>
2024-09-24 14:55:58 +0600

    Change styles on donation button and history button and added some content on blogh page

--- js/script.js
+++ js/script.js
@@ -1,13 +1,31 @@
 // Button Tab
+
+const historyButton = document.getElementById('history-btn');
+const donationButton = document.getElementById('donation-btn');
+
 document.getElementById('history-btn').addEventListener('click', function() {
+
+historyButton.classList.add('bg-button_bg', 'border-button_bg', 'text-primary');
+historyButton.classList.remove('bg-white', 'border-gray-400', 'text-secondary');
+
+donationButton.classList.remove('bg-button_bg', 'border-button_bg', 'text-primary');
+donationButton.classList.add('bg-white', 'border-gray-400', 'text-secondary' )
+
     showButtonSectionById('history-section');
     const history = document.getElementById('history-section');
     history.classList.remove('hidden');
+
 })
 
 
-
 document.getElementById('donation-btn').addEventListener('click', function() {
+
+    historyButton.classList.remove('bg-button_bg', 'border-button_bg', 'text-primary');
+historyButton.classList.add('bg-white', 'border-gray-400', 'text-secondary');
+
+donationButton.classList.add('bg-button_bg', 'border-button_bg', 'text-primary');
+donationButton.classList.remove('bg-white', 'border-gray-400', 'text-secondary' )
+
     showButtonSectionById('cards-section');
     const history = document.getElementById('history-section');
     history.classList.add('hidden');
@@ -20,14 +38,14 @@ document.getElementById('donation-btn').addEventListener('click', function() {
 function getInputValueById(id) {
     const inputValue = document.getElementById(id).value;
     const inputNumber = Number(inputValue);
-    console.log(inputNumber);
+    
     return inputNumber;
 }
 
 function getTextValueById(id) {
     const textValue = document.getElementById(id).innerText;
     const textNumber = parseFloat(textValue);
-    console.log(textNumber);
+    
     return textNumber;
 }
 
@@ -46,16 +64,19 @@ document.getElementById('noakhali-donate-btn').addEventListener('click', functio
 
     const noakhaliInput = document.getElementById('noakhali-input').value;
     const noakhaliInputMoney = parseFloat(noakhaliInput);
-    console.log(noakhaliInputMoney);
 
-    if (typeof noakhaliInputMoney === 'number' && noakhaliInputMoney > 0 ) {
+    const myBalance = getTextValueById('my-balance');
+    
+    
+
+    if (typeof noakhaliInputMoney === 'number' && noakhaliInputMoney > 0 && myBalance >= noakhaliInputMoney) {
         // Noakhali Balance
         const noakhaliBalance = getTextValueById('noakhali-amount');
         const noakhaliNewBalance = noakhaliBalance + noakhaliInputMoney;
         document.getElementById('noakhali-amount').innerText = noakhaliNewBalance;
 
         // My Balance
-        const myBalance = getTextValueById('my-balance');
+        // const myBalance = getTextValueById('my-balance');
         const remainingMyBalance = myBalance - noakhaliInputMoney;
         document.getElementById('my-balance').innerText = remainingMyBalance;
 
@@ -66,8 +87,8 @@ document.getElementById('noakhali-donate-btn').addEventListener('click', functio
         // History Section
         const now = new Date();
 const h2 = document.createElement('h2');
-h2.innerText = `${noakhaliInputMoney} Taka is Donated for Flood Relief in Noakhali, Bangladesh 
-${now}`
+h2.innerText = `"${noakhaliInputMoney} Taka is Donated for Flood Relief in Noakhali, Bangladesh" 
+"${now}"`
 h2.classList.add('font-bold', 'text-xl', 'p-3','border', 'border-gray-300','border-2', 'rounded-xl', 'my-6')
 document.getElementById('history-item').appendChild(h2);
 
@@ -83,16 +104,18 @@ document.getElementById('feni-donate-btn').addEventListener('click', function (e
 
     const feniInput = document.getElementById('feni-input').value;
     const feniInputMoney = parseFloat(feniInput);
-    console.log(feniInputMoney);
+    
 
-    if (typeof feniInputMoney === 'number' && feniInputMoney > 0 ) {
+    const myBalance = getTextValueById('my-balance');
+
+    if (typeof feniInputMoney === 'number' && feniInputMoney > 0 && myBalance >= feniInputMoney) {
         // Noakhali Balance
         const feniBalance = getTextValueById('feni-amount');
         const feniNewBalance = feniBalance + feniInputMoney;
         document.getElementById('feni-amount').innerText = feniNewBalance;
 
         // My Balance
-        const myBalance = getTextValueById('my-balance');
+        
         const remainingMyBalance = myBalance - feniInputMoney;
         document.getElementById('my-balance').innerText = remainingMyBalance;
 
@@ -103,8 +126,8 @@ document.getElementById('feni-donate-btn').addEventListener('click', function (e
         // History Section
         const now = new Date();
 const h2 = document.createElement('h2');
-h2.innerText = `${feniInputMoney} Taka is Donated for Flood Relief in Feni,Bangladesh 
-${now}`
+h2.innerText = `"${feniInputMoney} Taka is Donated for Flood Relief in Feni,Bangladesh" 
+"${now}"`
 h2.classList.add('font-bold', 'text-xl', 'p-3','border', 'border-gray-300','border-2', 'rounded-xl', 'my-6')
 document.getElementById('history-item').appendChild(h2);
 
@@ -120,16 +143,18 @@ document.getElementById('quota-movement-btn').addEventListener('click', function
 
     const quotaInput = document.getElementById('quota-movement-input').value;
     const quotaInputMoney = parseFloat(quotaInput);
-    console.log(quotaInputMoney);
+    
 
-    if (typeof quotaInputMoney === 'number' && quotaInputMoney > 0 ) {
+    const myBalance = getTextValueById('my-balance');
+
+    if (typeof quotaInputMoney === 'number' && quotaInputMoney > 0 && myBalance >= quotaInputMoney) {
         // Quota Balance
         const quotaBalance = getTextValueById('quota-amount');
         const quotaNewBalance = quotaBalance + quotaInputMoney;
         document.getElementById('quota-amount').innerText = quotaNewBalance;
 
         // My Balance
-        const myBalance = getTextValueById('my-balance');
+        
         const remainingMyBalance = myBalance - quotaInputMoney;
         document.getElementById('my-balance').innerText = remainingMyBalance;
 
@@ -140,8 +165,8 @@ document.getElementById('quota-movement-btn').addEventListener('click', function
         // History Section
         const now = new Date();
 const h2 = document.createElement('h2');
-h2.innerText = `${quotaInputMoney} Taka is Donated for  for Injured in the Quota Movement, Bangladesh 
-${now}`
+h2.innerText = `"${quotaInputMoney} Taka is Donated for  for Injured in the Quota Movement, Bangladesh" 
+"${now}"`
 h2.classList.add('font-bold', 'text-xl', 'p-3','border', 'border-gray-300','border-2', 'rounded-xl', 'my-6')
 document.getElementById('history-item').appendChild(h2);
 
